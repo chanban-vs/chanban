f72aec851afade94f4144f4d5b0f64dd009fa093
JunHyuk Moon <68009161+dreamjh20@users.noreply.github.com>
2023-05-23 22:32:27 +0900

    Revert "cb-3 create entity (#2)"
    This reverts commit c5537c49ab57155ff9a3477b0c454157bf77b9f0.

--- src/main/kotlin/vs/chanban/domain/topic/TopicRepository.kt
+++
@@ -1,8 +0,0 @@
-package vs.chanban.domain.topic
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TopicRepository: JpaRepository<Topic, Long> {
-}
