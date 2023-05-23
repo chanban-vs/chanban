9d01363f94697bdea815a3fa75c8e48d8d00c533
dreamjh20 <dreamjh20@gmail.com>
2023-05-23 22:27:00 +0900

    ADD: enable jpa

--- src/main/kotlin/vs/chanban/ChanBanApplication.kt
+++ src/main/kotlin/vs/chanban/ChanBanApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditin
 class ChanBanApplication
 
 fun main(args: Array<String>) {
