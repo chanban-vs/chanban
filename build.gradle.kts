f2d6b8fe26e375dfafd321bf8bc991fba369215e
JunHyuk Moon <68009161+dreamjh20@users.noreply.github.com>
2023-05-25 16:15:56 +0900

    dev (#6)
    * cb-3 create entity (#4)
    
    * ADD: JPA dependency
    
    * ADD: JPA dependency
    
    * ADD: JPA dependency
    
    * ADD: enable jpa
    
    * ADD: enable jpa
    
    * CREATE: BaseTimeListener
    created at, updated at listener
    
    * CREATE: BaseTimeListenerTest
    created at listener test
    
    * CREATE: comment entity
    
    * CREATE: comment repository
    
    * CREATE: comment service
    
    * CREATE: poll entity
    
    * CREATE: poll option enum
    
    * CREATE: poll repository
    
    * CREATE: poll service
    
    * CREATE: topic entity
    
    * CREATE: topic repository
    
    * CREATE: topic repository test
    
    * CREATE: topic service
    
    * cb 4 add mysql connection (#5)
    
    * ADD: connect mysql
    
    * ADD: environment value
    
    * ADD: mysql dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.mysql:mysql-connector-j")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
