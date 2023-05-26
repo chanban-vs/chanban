6baae7f93178a22b0f8e678dea9a5f103145744b
dreamjh20 <dreamjh20@gmail.com>
2023-05-26 14:06:07 +0900

    FIX: Comment entity

--- src/main/kotlin/vs/chanban/domain/comment/Comment.kt
+++ src/main/kotlin/vs/chanban/domain/comment/Comment.kt
@@ -3,20 +3,21 @@ package vs.chanban.domain.comment
 import jakarta.persistence.*
 import vs.chanban.domain.listener.BaseTimeListener
 
-@Entity(name = "comment")
-class Comment: BaseTimeListener() {
+@Entity
+@Table(name = "comment")
+class Comment(
     @Id
     @Column(name = "comment_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val commentId: Long? = null
+    val commentId: Long? = null,
 
     @ManyToOne
     @JoinColumn(name = "parent_comment_id")
-    val parentCommentId: Comment? = null
+    val parentCommentId: Comment? = null,
 
-    @Column(name = "comment_content")
-    val commentContent: String = ""
+    @Column(name = "comment_content", nullable = false)
+    val commentContent: String,
 
-    @Column(name = "userIp", length = 20, nullable = false)
-    val userIp: String = ""
-}
+    @Column(name = "user_ip", length = 20, nullable = false)
+    val userIp: String
+): BaseTimeListener()
