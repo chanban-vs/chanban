62f0d0adb3a8417f042bb6795381364075979ad4
dreamjh20 <dreamjh20@gmail.com>
2023-05-26 09:23:32 +0900

    FIX: Poll Entity

--- src/main/kotlin/vs/chanban/domain/poll/Poll.kt
+++ src/main/kotlin/vs/chanban/domain/poll/Poll.kt
@@ -5,21 +5,22 @@ import vs.chanban.domain.enum.poll.PollOption
 import vs.chanban.domain.listener.BaseTimeListener
 import vs.chanban.domain.topic.Topic
 
-@Entity(name = "poll")
-class Poll(topic: Topic, pollOption: PollOption, userIp: String): BaseTimeListener() {
+@Entity
+@Table(name = "poll")
+class Poll(
     @Id
     @Column(name = "poll_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val pollId: Long? = null
+    val pollId: Long? = null,
 
     @ManyToOne
     @JoinColumn(name = "topic_id", nullable = false)
-    val topicId: Topic? = null
+    val topicId: Topic,
 
     @Enumerated(EnumType.STRING)
     @Column(name = "poll_answer", length = 20, nullable = false)
-    val pollAnswer: PollOption? = null
+    val pollAnswer: PollOption,
 
     @Column(name = "user_ip", length = 20, nullable = false)
-    val userIp: String = ""
-}
+    val userIp: String
+): BaseTimeListener()
