80b9718f73e7fc4abd13e2fda544421526d50fbb
JunHyuk Moon <68009161+dreamjh20@users.noreply.github.com>
2023-05-28 00:47:31 +0900

    Cb 6 (#11)
    * DELETE: Duplicated test
    
    * ADD: Ip address data
    
    * CREATE: IpUtil object
    
    * CREATE: IpUtilTest
    retrieve ip address test
    
    * ADD: ip retrieving test api

--- src/main/kotlin/vs/chanban/controller/HelloController.kt
+++ src/main/kotlin/vs/chanban/controller/HelloController.kt
@@ -1,9 +1,11 @@
 package vs.chanban.controller
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import vs.chanban.common.exception.ChanbanBizException
+import vs.chanban.domain.support.util.ip.IpUtil
 
 @RestController
 class HelloController {
@@ -15,5 +17,10 @@ class HelloController {
     @GetMapping("/error-test")
     fun errorTest(): String {
         throw ChanbanBizException(HttpStatus.BAD_REQUEST ,"Error test")
+    }
+
+    @GetMapping("/ip-test")
+    fun retrieveIpAddressTest(request: HttpServletRequest): String {
+        return IpUtil.getClientIpAddress(request)
     }
 }
