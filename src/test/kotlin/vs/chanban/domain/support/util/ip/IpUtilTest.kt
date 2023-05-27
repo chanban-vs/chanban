a6597bf9972ecb3edfe416402c186f23218497f6
dreamjh20 <dreamjh20@gmail.com>
2023-05-28 00:32:27 +0900

    CREATE: IpUtilTest retrieve ip address test

---
+++ src/test/kotlin/vs/chanban/domain/support/util/ip/IpUtilTest.kt
@@ -0,0 +1,21 @@
+package vs.chanban.domain.support.util.ip
+
+import TestData.IpAddress.TEST_IP_ADDRESS
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.mock.web.MockHttpServletRequest
+
+class IpUtilTest {
+    @Test
+    @DisplayName("retrieve ip address")
+    fun testGetClientIpAddress_WithXForwardedForHeader() {
+        val request = MockHttpServletRequest()
+        request.addHeader("X-Forwarded-For", TEST_IP_ADDRESS)
+
+        val retrievedIpAddress = IpUtil.getClientIpAddress(request)
+
+        assertEquals(TEST_IP_ADDRESS, retrievedIpAddress)
+    }
+
+}
