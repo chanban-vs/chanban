2ad90eefffaaa3c94a98f1f6da510d7fee5f2412
dreamjh20 <dreamjh20@gmail.com>
2023-05-30 10:02:55 +0900

    ADD: findAllByTopicSubject

--- src/main/kotlin/vs/chanban/domain/topic/TopicRepository.kt
+++ src/main/kotlin/vs/chanban/domain/topic/TopicRepository.kt
@@ -1,8 +1,12 @@
 package vs.chanban.domain.topic
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import vs.chanban.domain.enum.topic.TopicSubject
 
 @Repository
 interface TopicRepository: JpaRepository<Topic, Long> {
+    fun findAllByTopicSubject(topicSubject: TopicSubject, pageable: Pageable): Page<Topic>
 }
