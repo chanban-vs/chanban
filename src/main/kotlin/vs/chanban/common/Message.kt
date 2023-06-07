a76fc1b2c1a1ac41876b71edfdc99717bced6534
dreamjh20 <dreamjh20@gmail.com>
2023-06-07 15:41:32 +0900

    ADD: User message

--- src/main/kotlin/vs/chanban/common/Message.kt
+++ src/main/kotlin/vs/chanban/common/Message.kt
@@ -7,7 +7,23 @@ class Message {
     }
 
     object Topic {
-        const val TOPIC_NOT_FOUND = "Topic with ID %d not found."
+        const val TOPIC_NOT_FOUND = "Topic with Id %d not found"
+    }
+
+    object User {
+        const val USER_NOT_FOUND = "User with %s %s not found"
+        const val WRONG_PASSWORD = "Wrong password"
+        const val TEMPORARY_USER_NOT_FOUND = "Temporary user with email %s not found"
+        const val WRONG_VERIFICATION_CODE = "Wrong verification code"
+        const val DUPLICATED_EMAIL = "Duplicated email %s"
+        const val INVALID_PASSWORD_BY_LENGTH = "Password should be at least %d characters long"
+    }
+    object Authentication {
+        const val UNAUTHORIZED = "Unauthorized"
+    }
+
+    object Mail {
+        const val VERIFY_SUBJECT = "Chanban email verification code"
     }
 
 }
