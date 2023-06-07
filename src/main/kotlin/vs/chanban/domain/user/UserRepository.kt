b8ca879179e60f5d14facc505b17f30dc6e806a9
dreamjh20 <dreamjh20@gmail.com>
2023-06-07 15:52:32 +0900

    ADD: findByUserEmail

--- src/main/kotlin/vs/chanban/domain/user/UserRepository.kt
+++ src/main/kotlin/vs/chanban/domain/user/UserRepository.kt
@@ -2,7 +2,10 @@ package vs.chanban.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface UserRepository: JpaRepository<User, Long> {
+    fun findByUserEmail(email: String): Optional<User>
+    fun existsByUserEmail(email: String): Boolean
 }
