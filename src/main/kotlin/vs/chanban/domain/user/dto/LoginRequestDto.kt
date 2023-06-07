e38ffefccf1bb252b5017f406336b264e3b9c314
dreamjh20 <dreamjh20@gmail.com>
2023-06-07 15:40:43 +0900

    CREATE: LoginRequestDto

---
+++ src/main/kotlin/vs/chanban/domain/user/dto/LoginRequestDto.kt
@@ -0,0 +1,11 @@
+package vs.chanban.domain.user.dto
+
+import jakarta.validation.constraints.NotBlank
+import vs.chanban.common.Message
+
+class LoginRequestDto(
+    @field:NotBlank(message = Message.Validation.CANNOT_BE_BLANK)
+    val email: String,
+    @field:NotBlank(message = Message.Validation.CANNOT_BE_BLANK)
+    val password: String
+)
