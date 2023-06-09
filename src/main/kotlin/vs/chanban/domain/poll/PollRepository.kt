d05777c1cbd896aa668846fbb4baa723b20bde4a
dreamjh20 <dreamjh20@gmail.com>
2023-06-09 20:37:02 +0900

    ADD: existsBycreatedBy

--- src/main/kotlin/vs/chanban/domain/poll/PollRepository.kt
+++ src/main/kotlin/vs/chanban/domain/poll/PollRepository.kt
@@ -2,7 +2,9 @@ package vs.chanban.domain.poll
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import vs.chanban.domain.user.User
 
 @Repository
 interface PollRepository: JpaRepository<Poll, Long> {
+    fun existsBycreatedBy(user: User): Boolean
 }
