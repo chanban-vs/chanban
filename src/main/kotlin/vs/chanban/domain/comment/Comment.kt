89fbc9616e82ae9100aa2a9b8b5e1edbac5d72af
dreamjh20 <dreamjh20@gmail.com>
2023-06-09 20:35:28 +0900

    ADD: Index

--- src/main/kotlin/vs/chanban/domain/comment/Comment.kt
+++ src/main/kotlin/vs/chanban/domain/comment/Comment.kt
@@ -3,14 +3,27 @@ package vs.chanban.domain.comment
 import jakarta.persistence.*
 import vs.chanban.domain.enum.poll.PollOption
 import vs.chanban.domain.listener.BaseTimeListener
+import vs.chanban.domain.topic.Topic
+import vs.chanban.domain.user.User
 
 @Entity
-@Table(name = "comment")
+@Table(name = "comment",
+    indexes = [
+        Index(columnList = "created_at", name = "comment_idx_1"),
+        Index(columnList = "updated_at", name = "comment_idx_2"),
+        Index(columnList = "topic_id", name = "comment_idx_3"),
+        Index(columnList = "poll_answer", name = "comment_idx_4")
+    ]
+)
 class Comment(
     @Id
     @Column(name = "comment_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val commentId: Long? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id", nullable = false)
+    val topicId: Topic,
 
     @ManyToOne
     @JoinColumn(name = "parent_comment_id")
@@ -23,6 +36,7 @@ class Comment(
     @Column(name = "comment_content", nullable = false)
     val commentContent: String,
 
-    @Column(name = "user_ip", length = 20, nullable = false)
-    val userIp: String
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    val createdBy: User
 ): BaseTimeListener()
--- src/main/kotlin/vs/chanban/domain/poll/Poll.kt
+++ src/main/kotlin/vs/chanban/domain/poll/Poll.kt
@@ -4,9 +4,18 @@ import jakarta.persistence.*
 import vs.chanban.domain.enum.poll.PollOption
 import vs.chanban.domain.listener.BaseTimeListener
 import vs.chanban.domain.topic.Topic
+import vs.chanban.domain.user.User
 
 @Entity
-@Table(name = "poll")
+@Table(name = "poll",
+    indexes = [
+        Index(columnList = "created_at", name = "poll_idx_1"),
+        Index(columnList = "updated_at", name = "poll_idx_2"),
+        Index(columnList = "topic_id", name = "poll_idx_3"),
+        Index(columnList = "created_by", name = "poll_idx_4"),
+        Index(columnList = "poll_answer", name = "poll_idx_5")
+    ]
+)
 class Poll(
     @Id
     @Column(name = "poll_id", nullable = false)
@@ -21,6 +30,7 @@ class Poll(
     @Column(name = "poll_answer", length = 20, nullable = false)
     val pollAnswer: PollOption,
 
-    @Column(name = "user_ip", length = 20, nullable = false)
-    val userIp: String
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    val createdBy: User
 ): BaseTimeListener()
