b7d3cf7c1ec131a04e9ef538cc085593737375a2
dreamjh20 <dreamjh20@gmail.com>
2023-06-09 20:37:10 +0900

    ADD: getIsPolled

--- src/main/kotlin/vs/chanban/domain/poll/PollService.kt
+++ src/main/kotlin/vs/chanban/domain/poll/PollService.kt
@@ -1,7 +1,13 @@
 package vs.chanban.domain.poll
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import vs.chanban.domain.user.User
 
 @Service
 class PollService(private val pollRepository: PollRepository) {
+    @Transactional(readOnly = true)
+    fun getIsPolled(user: User): Boolean {
+        return pollRepository.existsBycreatedBy(user)
+    }
 }
