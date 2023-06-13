abe80624380b9f82569eefed850217ad49bc3e98
dreamjh20 <dreamjh20@gmail.com>
2023-06-13 16:12:01 +0900

    FIX: allow null to validate field

--- src/main/kotlin/vs/chanban/domain/user/dto/AddTemporaryUserRequestDto.kt
+++ src/main/kotlin/vs/chanban/domain/user/dto/AddTemporaryUserRequestDto.kt
@@ -2,13 +2,14 @@ package vs.chanban.domain.user.dto
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import vs.chanban.common.Message
 import vs.chanban.domain.enum.account.role.AccountRole
 
 class AddTemporaryUserRequestDto(
     @field:NotBlank(message = Message.Validation.CANNOT_BE_BLANK)
     val email: String? = null,
-    @field:NotBlank(message = Message.Validation.CANNOT_BE_BLANK)
+    @field:NotNull(message = Message.Validation.CANNOT_BE_NULL)
     var password: String? = null,
 
     @field:JsonIgnore
