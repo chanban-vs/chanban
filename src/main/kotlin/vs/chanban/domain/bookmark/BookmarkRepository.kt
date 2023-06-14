a7e012000f37b658994fbf1b62d7e898fc31edf7
dreamjh20 <dreamjh20@gmail.com>
2023-06-14 21:09:09 +0900

    ADD: Jpa method

--- src/main/kotlin/vs/chanban/domain/bookmark/BookmarkRepository.kt
+++ src/main/kotlin/vs/chanban/domain/bookmark/BookmarkRepository.kt
@@ -1,8 +1,12 @@
 package vs.chanban.domain.bookmark
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import vs.chanban.domain.user.User
 
 @Repository
 interface BookmarkRepository: JpaRepository<Bookmark, Long> {
+    fun findBookmarksByCreatedByOrderByCreatedAtDesc(createdBy: User, pageable: Pageable): Page<Bookmark>
 }
