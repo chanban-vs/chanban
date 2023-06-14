fa2eb5b0ed2ab0620193a60c4a76813f71ebac8b
dreamjh20 <dreamjh20@gmail.com>
2023-06-14 21:48:45 +0900

    ADD: existsByCreatedByAndTopicId method

--- src/main/kotlin/vs/chanban/domain/bookmark/BookmarkRepository.kt
+++ src/main/kotlin/vs/chanban/domain/bookmark/BookmarkRepository.kt
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import vs.chanban.domain.topic.Topic
 import vs.chanban.domain.user.User
 
 @Repository
 interface BookmarkRepository: JpaRepository<Bookmark, Long> {
     fun findBookmarksByCreatedByOrderByCreatedAtDesc(createdBy: User, pageable: Pageable): Page<Bookmark>
+    fun existsByCreatedByAndTopicId(user: User, topic: Topic): Boolean
 }
