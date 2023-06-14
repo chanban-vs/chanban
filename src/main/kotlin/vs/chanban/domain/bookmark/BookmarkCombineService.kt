cfac6157f3b0571d5f5b50975d84cdfc06e142ac
dreamjh20 <dreamjh20@gmail.com>
2023-06-14 21:49:14 +0900

    ADD: isBookmarked method

--- src/main/kotlin/vs/chanban/domain/bookmark/BookmarkCombineService.kt
+++ src/main/kotlin/vs/chanban/domain/bookmark/BookmarkCombineService.kt
@@ -2,11 +2,16 @@ package vs.chanban.domain.bookmark
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import vs.chanban.common.Message.Authentication.FORBIDDEN
+import vs.chanban.common.exception.ChanbanBizException
 import vs.chanban.configuration.PaginationConfig
 import vs.chanban.domain.bookmark.dto.AddBookmarkRequestDto
 import vs.chanban.domain.bookmark.dto.AddBookmarkResponseDto
+import vs.chanban.domain.bookmark.dto.IsBookmarkedResponseDto
+import vs.chanban.domain.topic.Topic
 import vs.chanban.domain.topic.TopicService
 import vs.chanban.domain.topic.dto.TopicPreviewResponseDto
 import vs.chanban.domain.user.User
@@ -25,7 +30,6 @@ class BookmarkCombineService(
         return AddBookmarkResponseDto.of(bookmark)
     }
 
-    @Transactional
     fun getBookmarkPage(user: User, page: Int?, pageSize: Int?): Page<TopicPreviewResponseDto> {
         val currentPage = page ?: paginationConfig.defaultPage
         val currentPageSize = pageSize ?: paginationConfig.defaultPageSize
@@ -35,5 +39,25 @@ class BookmarkCombineService(
         return bookmarkPage.map { bookmark ->
             TopicPreviewResponseDto.of(bookmark.topicId)
         }
+    }
+
+    fun isBookmarked(user: User, topicId: Long): IsBookmarkedResponseDto {
+        val topic: Topic = topicService.getTopic(topicId)
+
+        return IsBookmarkedResponseDto.of(bookmarkService.isBookmarked(user, topic))
+    }
+
+    @Transactional
+    fun deleteBookmark(user: User, bookmarkId: Long) {
+        val bookmark: Bookmark = bookmarkService.getBookmark(bookmarkId)
+
+        // user only who added this bookmark is able to delete
+        if (user == bookmark.createdBy) {
+            bookmarkService.deleteBookmark(bookmarkId)
+        } else {
+            throw ChanbanBizException(HttpStatus.FORBIDDEN, FORBIDDEN)
+        }
+
+        return
     }
 }
