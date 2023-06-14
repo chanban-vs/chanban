72327c9075c6a2963ea824eb810f9aba78af63b3
dreamjh20 <dreamjh20@gmail.com>
2023-06-14 21:48:18 +0900

    ADD: getBookmark method

--- src/main/kotlin/vs/chanban/domain/bookmark/BookmarkService.kt
+++ src/main/kotlin/vs/chanban/domain/bookmark/BookmarkService.kt
@@ -2,8 +2,13 @@ package vs.chanban.domain.bookmark
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import vs.chanban.common.Message.Bookmark.BOOKMARK_NOT_FOUND
+import vs.chanban.common.constant.Constant.Common.ID
+import vs.chanban.common.exception.ChanbanBizException
+import vs.chanban.domain.topic.Topic
 import vs.chanban.domain.user.User
 
 @Service
@@ -13,6 +18,14 @@ class BookmarkService(private val bookmarkRepository: BookmarkRepository) {
         return bookmarkRepository.save(bookmark)
     }
 
+    @Transactional(readOnly = true)
+    fun getBookmark(bookmarkId: Long): Bookmark {
+        return bookmarkRepository.findById(bookmarkId).orElseThrow {
+            ChanbanBizException(HttpStatus.BAD_REQUEST, BOOKMARK_NOT_FOUND.format(ID, bookmarkId))
+        }
+    }
+
+    @Transactional(readOnly = true)
     fun getBookmarkPage(user: User, pageable: Pageable): Page<Bookmark> {
         return bookmarkRepository.findBookmarksByCreatedByOrderByCreatedAtDesc(user, pageable)
     }
