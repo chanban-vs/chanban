f374b241af9a085f67fba48a239c09ab57a116eb
dreamjh20 <dreamjh20@gmail.com>
2023-06-14 15:19:43 +0900

    CREATE: AddCommentRequestDto

---
+++ src/main/kotlin/vs/chanban/domain/comment/dto/AddCommentRequestDto.kt
@@ -0,0 +1,28 @@
+package vs.chanban.domain.comment.dto
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import vs.chanban.common.Message
+import vs.chanban.domain.comment.Comment
+import vs.chanban.domain.topic.Topic
+import vs.chanban.domain.user.User
+
+class AddCommentRequestDto(
+    @field:NotNull(message = Message.Validation.CANNOT_BE_NULL)
+    val topicId: Long? = null,
+    // parent comment id could be null
+    val parentCommentId: Long? = null,
+    @field:NotBlank(message = Message.Validation.CANNOT_BE_BLANK)
+    val comment: String? = null,
+    @field:NotBlank(message = Message.Validation.CANNOT_BE_BLANK)
+    val pollAnswer: String? = null,
+
+    @field:JsonIgnore
+    var createdBy: User? = null,
+    @field:JsonIgnore
+    var topic: Topic? = null,
+    @field:JsonIgnore
+    var parentComment: Comment? = null
+) {
+}
