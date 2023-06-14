9bc81c27cb40f082ff06d03889052efa4929c348
dreamjh20 <dreamjh20@gmail.com>
2023-06-14 21:09:24 +0900

    ADD: add bookmark, get bookmark method

--- src/main/kotlin/vs/chanban/domain/bookmark/BookmarkService.kt
+++ src/main/kotlin/vs/chanban/domain/bookmark/BookmarkService.kt
@@ -1,7 +1,19 @@
 package vs.chanban.domain.bookmark
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import vs.chanban.domain.user.User
 
 @Service
+@Transactional
 class BookmarkService(private val bookmarkRepository: BookmarkRepository) {
+    fun addBookmark(bookmark: Bookmark): Bookmark {
+        return bookmarkRepository.save(bookmark)
+    }
+
+    fun getBookmarkPage(user: User, pageable: Pageable): Page<Bookmark> {
+        return bookmarkRepository.findBookmarksByCreatedByOrderByCreatedAtDesc(user, pageable)
+    }
 }
