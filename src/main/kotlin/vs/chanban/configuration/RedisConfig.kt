d839359d0ff170fa77c15592f817d23ed3c56df5
JunHyuk Moon <68009161+dreamjh20@users.noreply.github.com>
2023-06-19 11:43:06 +0900

    Cb 17 (#22)
    * CREATE: Dockerfile
    
    * ADD: properties

--- src/main/kotlin/vs/chanban/configuration/RedisConfig.kt
+++ src/main/kotlin/vs/chanban/configuration/RedisConfig.kt
@@ -1,5 +1,6 @@
 package vs.chanban.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -9,9 +10,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
+    @Value("\${spring.redis.host}")
+    lateinit var redisHost: String
+
+    @Value("\${spring.redis.port}")
+    lateinit var redisPort: String
+
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory()
+        return LettuceConnectionFactory(redisHost, redisPort.toInt())
     }
     @Bean
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
